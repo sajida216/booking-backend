ae21b8016841f350c3d5fd1253d2a86123b19747
sajida216 <ayishathsajidha@gmail.com>
2025-06-30 00:00:05 +0530

    Updated server file and port binding

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT ||5000;
 const cors = require('cors');
 
 app.use(cors());
